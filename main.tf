4f812296ac18370aa9df6dd37e8bf66729827845
Saleem23432 <mohammedsaleemahmed9036@gmail.com>
2025-11-13 13:07:38 +0530

    Change instance type from t2.micro to t3.micro

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ region = "us-east-1"
 resource "aws_instance" "three" {
   count         = 2
   ami           = "ami-0cae6d6fe6048ca2c"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "dev-server"
