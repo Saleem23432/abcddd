ff7f8e9c56ca92049de67d8bfac603a9c6933d8f
Saleem23432 <mohammedsaleemahmed9036@gmail.com>
2025-11-13 12:30:10 +0530

    Update AWS instance configuration in main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "three" {
-  count         = 1
-  ami           = "ami-00ca32bbc84273381"
-  instance_type = "t2.micro"
+  count         = 2
+  ami           = "ami-0cae6d6fe6048ca2c "
+  instance_type = "t3.micro"
 
   tags = {
     Name = "dev-server"
